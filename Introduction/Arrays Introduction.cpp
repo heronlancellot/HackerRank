86b1b0d06c0514bd038dfdd83ee03ccd906775e2
Heron Lancellot <heronlancellot@id.uff.br>
2021-12-07 18:52:03 -0300

    Introduction Exercise

---
+++ Introduction/Arrays Introduction.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int i;
+    scanf("%d", &i);
+    int *n = (int *) malloc(sizeof(int) * i);
+    int *x = (int *) malloc(sizeof(int) * i);
+    
+    for(int cont = 0; cont < i; cont ++){
+        scanf("%d", &n[cont]);
+    }
+    for(int cont = 0; cont < i; cont ++){
+        x[cont] = n[i-cont-1];
+        printf("%d ", x[cont]);
+    }
+
+
+    return 0;
+}
