292342fd1580615cecea607016ee856337782e33
Heron Lancellot <heronlancellot@id.uff.br>
2021-12-07 21:55:18 -0300

    Introduction Exercises

--- Introduction/Arrays Introduction.cpp
+++ Introduction/Arrays Introduction.cpp
@@ -8,9 +8,10 @@ using namespace std;
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
     int i;
-    scanf("%d", &i);
     int *n = (int *) malloc(sizeof(int) * i);
     int *x = (int *) malloc(sizeof(int) * i);
+    scanf("%d", &i);
+
     
     for(int cont = 0; cont < i; cont ++){
         scanf("%d", &n[cont]);
